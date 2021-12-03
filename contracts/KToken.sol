06c17139d25eab2ebaf1037ad5252c051cb24800
Martin Kim <martin.kim@kakaocorp.com>
2021-12-03 15:00:57 +0900

    기본 구조 세팅

---
+++ contracts/KToken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.6;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract KToken is ERC20 {
+    uint256 public initialSupply = 1000000 * 10 ** 18;
+
+    constructor () ERC20("ZZM Token", "ZZM") {
+        _mint(msg.sender, initialSupply); // 주의!
+    }
+
+
+}
---
+++ frontend/README.md
@@ -0,0 +1,6 @@
+sample app
+
+```
+# https://evergreen.segment.com/introduction/getting-started
+npm install evergreen-ui
+```
